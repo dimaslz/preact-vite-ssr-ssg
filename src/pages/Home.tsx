ce55009f3f4033e62b9a3f069411a681c37590a2
dimaslz <me@dimaslz.io>
2023-05-29 09:52:17 +0200

    fix: add links demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,23 @@ import { useState } from 'preact/hooks'
 import preactLogo from '../assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './home.css'
+import { Link } from "preact-router/match";
 
 const Home: FunctionalComponent = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="flex w-full justify-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+          <img src={preactLogo} className="logo preact" alt="Preact logo" />
         </a>
       </div>
       <h1>Vite + Preact + SSR</h1>
-      <div class="card">
+      <div className="card space-y-4">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,9 +28,9 @@ const Home: FunctionalComponent = () => {
           Edit <code>src/pages/Home.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <div>
+        <Link href="/other">Go to /other page</Link>
+      </div>
     </>
   )
 }
--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -1,12 +1,16 @@
 import { FunctionalComponent } from "preact";
+import { Link } from "preact-router/match";
 
 const Other: FunctionalComponent = () => {
   return (
-    <>
+    <div className="space-y-4">
       <div>
-        <h1>OTHER</h1>
+        <h1>Other page</h1>
       </div>
-    </>
+      <div>
+        <Link href="/">Go to Home page</Link>
+      </div>
+    </div>
   )
 }
 
